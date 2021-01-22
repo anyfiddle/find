85a1005674523ad5521b1af7a7402d84ab82fde1
Joji Augustine <jojimail@gmail.com>
2021-01-22 22:18:38 +0530

    Add env to run

--- run-vm.sh
+++ run-vm.sh
@@ -1,7 +1,9 @@
 exec docker run \
     -ti \
-    -v $(pwd)/rootfs.ext4:/rootfs.ext4 \
-    -p 8022:22 \
-    -p 8080:8080 \
     --privileged \
-    find --root-drive=/rootfs.ext4
+    -v $(pwd)/../vmdisk:/disk \
+    -p 8022:22 \
+    -p 9876:9876 \
+    -p 8080:8080 \
+    -e ROOTFS_PATH=/disk/image.ext4
+    anyfiddle/find:0.0.2
